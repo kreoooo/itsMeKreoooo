9eb4b31deaf832c93ec69c969610c61e6e3dd0a8
kreoooo <129646630+kreoooo@users.noreply.github.com>
2024-03-29 18:09:51 +0100

    Delete Battleships-Server-v4 directory

--- Battleships-Server-v4/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module pl.kielce.tu.battleshipsserverv4 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires jcl.over.slf4j;
-
-
-    opens pl.kielce.tu.battleshipsserverv4 to javafx.fxml;
-    exports pl.kielce.tu.battleshipsserverv4;
-}
