37ace19a5ea7dc8fdc7b2e95a38d7ed378cb02c5
kreoooo <129646630+kreoooo@users.noreply.github.com>
2024-03-29 18:09:40 +0100

    Delete Battleships-Client-v4 directory

--- Battleships-Client-v4/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module pl.kielce.tu.battleshipsclientv4 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens pl.kielce.tu.battleshipsclientv4 to javafx.fxml;
-    exports pl.kielce.tu.battleshipsclientv4;
-}
